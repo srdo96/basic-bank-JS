68d8aa078e9dd45de2d59c9b6bd1634f3a13b6dd
Sakib Rahman <seu.sakib@gmail.com>
2022-02-10 17:26:34 +0600

    add withdraw

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,4 @@
+// deposit area
 document
   .getElementById("deposite-button")
   .addEventListener("click", function () {
@@ -19,4 +20,23 @@ document
     } else {
       document.getElementById("deposite-area").style.backgroundColor("red");
     }
+  });
+
+//   withdraw area
+
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawInput = document.getElementById("withdraw-input");
+    const newWithdrawAmount = parseInt(withdrawInput.value);
+
+    const withdrawAmountTotalText = document.getElementById(
+      "withdraw-amount-total"
+    );
+    const withdrawAmountTotal = parseInt(withdrawAmountTotalText.innerText);
+    const currentWithdrawAmount = withdrawAmountTotal + newWithdrawAmount;
+    withdrawAmountTotalText.innerText = currentWithdrawAmount;
+    //   balance
+    const totalBalanceText = document.getElementById("total-balance");
+    let totalBalance = parseInt(balance.innerText);
   });
